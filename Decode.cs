2c86689b2b3bbf40681ebb13ab67bd281e3ab0dd
De1m0s114 <yegor1002@gmail.com>
2021-04-09 00:13:43 +0300

    Fix)
    changes

--- Decode.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-public class Decode : Code
-{
-    public int v = Convert.ToInt32(Math.Pow(c, d)) % f; //Декодировка
-}
-
---
+++ RSA/Decode.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class Decode : Code
+{
+    public int v = Convert.ToInt32(Math.Pow(c, d)) % f; //Декодировка
+}
+
